b4a47b086cf6f0da76c63878f6a0fdfe0fa0b17f
shahriarlabib000 <100587814+shahriarlabib000@users.noreply.github.com>
2022-08-08 00:28:43 +0600

    Add files via upload

---
+++ 69420_counter.py
@@ -0,0 +1,22 @@
+import time 
+print("Printing to 69420")
+x=float(input("Any number from -∞ to 69419: "))
+if x==69:
+    print("nice")
+    time.sleep(2)
+while x>=69420:
+    print("You retarded?")
+    x=float(input("Any number between -∞ and 69419 you dumbfuck: "))
+    if x==69:
+        print("Nice job motherfucker")
+        time.sleep(2)
+while x<69420:
+          x=(x+1)
+          print(x)
+          if x >=(69420):
+                 break
+print("nice") 
+
+ 
+    
+   
